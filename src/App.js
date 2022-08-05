b78b7ecf7435d82e791ebc9b86f394dd4c1923ce
Akshay <rathodak315@gmail.com>
2022-08-05 16:48:28 +0530

    updated fonts and color palets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "../src/css/main.scss";
+import ModuleSection from "./components/Modules-section";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ModuleSection />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Modules-section/index.js
+++ src/components/Modules-section/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ModuleSection = () => {
-  return <div>ModuleSection</div>;
+  return (
+    <div className="module-section">
+      <div className="module-header">
+        <span>12</span>
+        MODULES
+      </div>
+      <div className="module-desc">
+        Modules are designed by our Experts to help you learn every aspect of
+        Investment & Stock Trading in very simple ways.
+      </div>
+    </div>
+  );
 };
 
 export default ModuleSection;

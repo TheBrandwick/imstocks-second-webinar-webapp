ed33d28543a3e222e515157f63e2a8fa92b08ef7
Akshay <rathodak315@gmail.com>
2022-08-05 16:24:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "../src/css/main.scss";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/components/Modules-section/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModuleSection = () => {
+  return <div>ModuleSection</div>;
+};
+
+export default ModuleSection;

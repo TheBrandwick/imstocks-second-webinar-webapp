c5539af3de3edaf32b390fb2459a58f87923340b
Rajat <robertdoweny2301@gmail.com>
2022-08-07 23:53:59 +0530

    added Sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "../src/css/main.scss";
 import Footer from "./components/Footer";
+import MasterClass from "./components/MasterClass";
 import ModuleSection from "./components/Modules-section";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <MasterClass/>
       <ModuleSection />
       <Footer />
     </div>
